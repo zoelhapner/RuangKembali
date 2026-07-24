5cf6909ffa49ab6aadd4a55977603500f34d69e3
zoelhapner <achmadzulkiflinurrochim@gmail.com>
2026-07-24 16:47:08 +0700

    tambah akuntansi

--- resources/views/accounting/index.blade.php
+++ resources/views/accounting/index.blade.php
@@ -62,20 +62,44 @@
             </div>
         </div>
     </div>
+@can('tambah akun-akuntansi')
+<a href="{{ route('accounting.create') }}"
+   class="mobile-fab d-md-none">
+
+    <svg xmlns="http://www.w3.org/2000/svg"
+         width="26"
+         height="26"
+         viewBox="0 0 24 24"
+         stroke-width="2"
+         stroke="currentColor"
+         fill="none"
+         stroke-linecap="round"
+         stroke-linejoin="round">
+
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <line x1="12" y1="5" x2="12" y2="19"/>
+        <line x1="5" y1="12" x2="19" y2="12"/>
+
+    </svg>
+
+</a>
+@endcan
 @endsection
 
 @push('js')
 <script>
     $(function() {
+        const isMobile = window.innerWidth < 576;
         const table = $('#tableAccounts').DataTable({
             scrollY: '500px',
             scrollX: true,
             scrollCollapse: true,
-                fixedColumns: {
+                fixedColumns: !isMobile ? {
                     leftColumns: 3
-                },
+                } : false,
             serverSide: true,
             processing: true,
+            responsive: false,
             ajax: '{{ route("accounting.index") }}',
             columns: [
                 { data: 'DT_RowIndex', name: 'DT_RowIndex', orderable: false, searchable: false },
